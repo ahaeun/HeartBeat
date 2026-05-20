e0abce34d872fb017ccd395b350f2f63d1f09b50
he.yang <he.yang@pitap.at>
2026-05-20 15:54:14 +0900

    first commit

---
+++ haeun/src/test/java/com/example/haeun/HaeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
